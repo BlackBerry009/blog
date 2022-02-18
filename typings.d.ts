d346446f2d4237b714e0a7a0bb01c6a6ba4d5278
罗云贵 <vae.luo@huolala.cn>
2022-02-18 18:27:10 +0800

    init

---
+++ typings.d.ts
@@ -0,0 +1,13 @@
+declare module '*.css';
+declare module '*.less';
+declare module '*.png';
+declare module '*.svg' {
+  export function ReactComponent(
+    props: React.SVGProps<SVGSVGElement>,
+  ): React.ReactElement;
+  const url: string;
+  export default url;
+}
+
+// const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+//   export default content;
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    dedupe: ['classnames'],
+    alias: [
+      { find: '@', replacement: path.join(__dirname, './src') }
+    ],
+  },
+})
