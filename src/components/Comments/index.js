18ba42ab6911642be6a3adccada9db99a50aa8ea
BlackBerry009 <765042894@qq.com>
2022-10-10 17:12:27 +0800

    add comments feature

---
+++ src/components/Comments/index.js
@@ -0,0 +1,27 @@
+import Giscus from '@giscus/react';
+import React from 'react';
+
+export const Comment = () => {
+  return (
+    <div style={{ paddingTop: 50 }}>
+      <Giscus
+        id="comments"
+        repo="BlackBerry009/blackberry009.github.io"
+        repoId="R_kgDOIFWOfg"
+        category="Announcements"
+        categoryId="DIC_kwDOIFWOfs4CR5M9"
+        mapping="title"
+        strict="1"
+        term="Welcome to @giscus/react component!"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="bottom"
+        theme="preferred_color_scheme"
+        lang="zh-CN"
+        loading="lazy"
+      />
+    </div>
+  );
+};
+
+export default Comment;
