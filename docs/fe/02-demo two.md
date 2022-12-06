c167b17f08774796879493101c6663fce7615cbc
BlackBerry009 <765042894@qq.com>
2022-12-06 19:05:52 +0800

    test search feature

--- docs/fe/02-demo two.md
+++
@@ -1,5 +0,0 @@
----
-sidebar_label: 测试2
----
-
-this is a 02demo
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,32 +1,32 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'BlackBerry\'s Site',
-  tagline: 'record some notes',
-  url: 'https://blackberry009.github.io',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  title: "BlackBerry's Site",
+  tagline: "record some notes",
+  url: "https://blackberry009.github.io",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'blackberry009', // Usually your GitHub org/user name.
-  projectName: 'blackberry009.github.io', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  organizationName: "blackberry009", // Usually your GitHub org/user name.
+  projectName: "blackberry009.github.io", // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
@@ -40,7 +40,7 @@ const config = {
           //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -64,25 +64,46 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'BlackBerry\'s Site',
+        title: "BlackBerry's Site",
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: '前端',
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "前端",
           },
-          {to: '/blog', label: '文章', position: 'left'},
+          { to: "/blog", label: "文章", position: "left" },
           {
-            href: 'https://github.com/BlackBerry009/BlackBerry.site',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/BlackBerry009/BlackBerry.site",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "QGOTOEP4L0",
+
+        // Public API key: it is safe to commit it
+        apiKey: "f9a46726f58cc9d9f11a98df152feae3",
+
+        indexName: "test",
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: "external\\.com|domain\\.com",
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        // searchPagePath: "search",
       },
     }),
 };
