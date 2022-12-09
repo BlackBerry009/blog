5b357d14f143124bdc62630e5670694d4511983e
BlackBerry009 <765042894@qq.com>
2022-12-09 16:58:04 +0800

    fix dark mode at home

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -19,9 +19,9 @@ const Hero: FC = memo(() => {
         ></img>
         <div className="z-10  max-w-screen-lg px-4 lg:px-0">
           <div className="flex flex-col items-center gap-y-6 rounded-xl bg-gray-800/40 p-6 text-center shadow-lg backdrop-blur-sm">
-            <h1 className="text-4xl font-bold text-white sm:text-5xl lg:text-7xl">
+            <p className="text-4xl font-bold text-white sm:text-5xl lg:text-7xl">
               {name}
-            </h1>
+            </p>
             {description}
             <div className="flex gap-x-4 text-neutral-100">
               <Socials />
--- src/components/Sections/Resume/TimelineItem.tsx
+++ src/components/Sections/Resume/TimelineItem.tsx
@@ -5,9 +5,9 @@ import {TimelineItem} from '../../../data/dataDef';
 const TimelineItemCmp: FC<{item: TimelineItem}> = memo(({item}) => {
   const {title, date, location, content} = item;
   return (
-    <div className="flex flex-col pb-8 text-center last:pb-0 md:text-left">
+    <div className="flex flex-col pb-8 text-center last:pb-0 md:text-left text-neutral-800">
       <div className="flex flex-col pb-4">
-        <h2 className="text-xl font-bold">{title}</h2>
+        <div className="text-xl font-bold">{title}</div>
         <div className="flex items-center justify-center gap-x-2 md:justify-start">
           <span className="flex-1 text-sm font-medium italic sm:flex-none">{location}</span>
           <span>•</span>
