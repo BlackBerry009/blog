edbc2f79bcc74e2fcf192dab93ea92f7eb8cd7d9
BlackBerry009 <765042894@qq.com>
2022-12-10 10:44:19 +0800

    update home style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        zoom: {
+          '0%': {
+            transform: 'scale(1)'
+          },
+          '100%': {
+            transform: 'scale(1.2)'
+          }
+        }
+      },
+      animation: {
+        zoomIn: '1s linear 0s infinite alternate zoom'
+      }
+    },
   },
   plugins: [],
   corePlugins: {
