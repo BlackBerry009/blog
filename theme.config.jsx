ccdceceb0e7656dec50f84b19a83a122da2825d3
M <vae.luo@huolala.cn>
2023-02-28 11:44:52 +0800

    init

---
+++ theme.config.jsx
@@ -0,0 +1,15 @@
+export default {
+  footer: <p></p>,
+  head: ({ title, meta }) => (
+    <>
+      {meta.description && (
+        <meta name="description" content={meta.description} />
+      )}
+      {meta.tag && <meta name="keywords" content={meta.tag} />}
+      {meta.author && <meta name="author" content={meta.author} />}
+    </>
+  ),
+  readMore: 'Read More →',
+  postFooter: null,
+  darkMode: true,
+}
