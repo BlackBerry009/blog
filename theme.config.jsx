f39d73d6d3ae921b6e268946941a541c50175e3b
BlackBerry009 <765042894@qq.com>
2023-03-13 22:58:26 +0800

    add icons

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,13 +1,27 @@
+import { AiFillGithub } from 'react-icons/ai'
+import { BiRss } from 'react-icons/bi'
+
 const YEAR = new Date().getFullYear()
 
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © BlackBerry.
-      <a href="/feed.xml">RSS</a>
+      <a href="/feed.xml">
+        <BiRss />
+      </a>
+      <a
+        href="https://github.com/BlackBerry009/blog"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <AiFillGithub />
+      </a>
       <style jsx>{`
         a {
           float: right;
+          margin-left: 20px;
+          font-size: 16px;
         }
         @media screen and (max-width: 480px) {
           article {
@@ -32,5 +46,5 @@ export default {
   darkMode: true,
   cusdis: {
     appId: '1137a882-35a0-464a-9d29-b45730be0357',
-  }
+  },
 }
