4a535b147dd8f6ba586f0725650db9f7cc335f14
BlackBerry009 <765042894@qq.com>
2023-03-30 23:26:38 +0800

    chore: update dep

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
