fc017d6f5228779c126380ac5ed867418c2b5bae
BlackBerry009 <765042894@qq.com>
2024-10-09 10:47:26 +0800

    change rss

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -3,14 +3,23 @@ const path = require('path')
 const RSS = require('rss')
 const matter = require('gray-matter')
 const MarkdownIt = require('markdown-it')
+const Feed = require('feed').Feed
 
 const md = new MarkdownIt()
 
 async function generate() {
-  const feed = new RSS({
+  const feed = new Feed({
     title: 'BlackBerry',
-    site_url: 'https://blog.blackberry009.top',
-    feed_url: 'https://blog.blackberry009.top/feed.xml',
+    description: 'feedId:66321001434793986+userId:63034699812466688',
+    favicon: 'https://avatars.githubusercontent.com/u/42471741?v=4',
+    link: 'https://blog.blackberry009.top',
+    feedLinks: {
+      json: 'https://blog.blackberry009.top/feed.xml',
+    },
+    author: {
+      name: "Blackberry",
+      email: "baymax.lyg@gmail.com",
+    }
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
@@ -26,7 +35,7 @@ async function generate() {
       allPosts.push({
         title: frontmatter.data.title,
         url: '/posts/' + name.replace(/\.mdx?/, ''),
-        date: frontmatter.data.date,
+        updated: frontmatter.data.date,
         description: md.render(frontmatter.content),
         categories: frontmatter.data.tag.split(', '),
         author: frontmatter.data.author,
@@ -36,9 +45,12 @@ async function generate() {
 
   allPosts.sort((a, b) => new Date(b.date) - new Date(a.date))
   allPosts.forEach((post) => {
-    feed.item(post)
+    feed.addItem(post)
   })
-  await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
+  // console.log(feed);
+  // console.log(feed.rss2());
+  // console.log(feed.json1());
+  await fs.writeFile('./public/feed.xml', feed.rss2())
 }
 
 generate()
